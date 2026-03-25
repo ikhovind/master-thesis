ad7c52e08fd6059ebe941384470571e7abb5ff15
Ingebrigt Hovind <ing.hovind@hotmail.com>
2026-03-25 11:20:53 +0100

    Delete unused files

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-pdflatex -synctex=1 -interaction=nonstopmode "main".tex
-rm -r -f *.aux *.bcf *.lof *.log *.lot *.out *.xml *.toc *.fls *.gz *.fdb_latexmk
-
-# rm main.fdb_latexmk
-# rm *.fls
-# rm *.synctex.gz
